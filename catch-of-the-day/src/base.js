3d8ce31eaac299bc7df3a730521f681afd12c6f3
George DuMontier <gdumonti@meredith.com>
2019-02-27 15:30:26 -0600

    update

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -1,14 +1,7 @@
 import Rebase from "re-base";
 import firebase from "firebase";
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyCk_T5_6fSVPzdC5aS5iVXYfCCFw8cO1oM",
-  authDomain: "catch-of-the-day-c5b6a.firebaseapp.com",
-  databaseURL: "https://catch-of-the-day-c5b6a.firebaseio.com",
-  projectId: "catch-of-the-day-c5b6a",
-  storageBucket: "catch-of-the-day-c5b6a.appspot.com",
-  messagingSenderId: "70806389047"
-});
+const firebaseApp = firebase.initializeApp({});
 
 const base = Rebase.createClass(firebaseApp.database());
 
